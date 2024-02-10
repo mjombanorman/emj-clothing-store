8098015df18555b08b7f39e70c4115ca45414c3c
mjombanorman <mjomban@gmail.com>
2024-02-10 11:51:13 +0300

    update menu-items to navigate withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,17 @@
 import "./menu-item.styles.scss";
-const MenuItem = ({ title, imageUrl,size }) => {
+import { withRouter } from "react-router-dom";
+
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      />
 
-      <div  style={{ backgroundImage: `url(${imageUrl})`}} className="background-image"/>
-
-     
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
@@ -14,4 +20,4 @@ const MenuItem = ({ title, imageUrl,size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,10 @@
 import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
-
 const HomePage = () => {
   return (
     <div className="homepage">
-   <Directory/>
+      <Directory/>
     </div>
   );
 };
