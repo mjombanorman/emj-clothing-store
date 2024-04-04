9aa6948963b196869767c799659e0f95f8835289
mjombanorman <mjomban@gmail.com>
2024-04-04 22:39:04 +0300

    Structured the app to independent reducers and selector for shop,directory and collections

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistore } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistore}>
+          <App />
+        </PersistGate>
+      
       </BrowserRouter>
     </React.StrictMode>
   </Provider>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,15 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
-//import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
 import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import { selectCollections } from "../../redux/shop/shop.selectors";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+  <CollectionsOverview/>
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    ); //<h2>SHOP</h2>;
-  }
-}
 
 export default ShopPage;
